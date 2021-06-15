3fec228087229bf685ef4d3c6e2edc798a6a3234
Stuart Fong <stuartbfong@gmail.com>
2021-06-15 18:00:16 -0400

    Update ApplePay.js

--- src/Checkout/ApplePay.js
+++ src/Checkout/ApplePay.js
@@ -1,67 +1,103 @@
-import { useEffect, useState } from "react";
-import { withRouter } from "react-router";
-import { PaymentRequestButtonElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import React, {useEffect, useState} from 'react';
+import {withRouter} from 'react-router-dom';
+import {PaymentRequestButtonElement, useStripe, useElements} from '@stripe/react-stripe-js';
+import StatusMessages, {useMessages} from './StatusMessages';
 
+const ApplePay = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [paymentRequest, setPaymentRequest] = useState(null);
+  const [messages, addMessage] = useMessages();
 
-export default function ApplePay({ order }){
-    const stripe = useStripe()
-    const elements = useElements()
-    const [paymentRequest, setPaymentRequest] = useState(null)
+  useEffect(() => {
+    if (!stripe || !elements) {
+      return;
+    }
 
-    useEffect(() => {
-        if(!stripe || !elements) return
+    const pr = stripe.paymentRequest({
+      country: 'US',
+      currency: 'usd',
+      total: {
+        label: 'Demo total',
+        amount: 1999,
+      },
+      requestPayerName: true,
+      requestPayerEmail: true,
+    });
 
-        const pr = stripe.paymentRequest({
-            currency: "cad",
-            country: "CA",
-            requestPayerEmail: true,
-            requestPayerName: true,
-            total: {
-                label: "Items (TESTING): ",
-                amount: 90
-            }
-        })
-        
-        pr.canMakePayment().then((result) => {
-            if(result){
-                setPaymentRequest(pr) 
-            }
-        })
-        pr.on("paymentmethod", async (e) => {
-            const { clientSecret } = await fetch("/create-payment-intent", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    paymentMethodType: "card",
-                    currency: "cad"
-                })
-            }).then(r => r.json())
+    // Check the availability of the Payment Request API.
+    pr.canMakePayment().then(result => {
+      if (result) {
+        setPaymentRequest(pr);
+      }
+    });
 
-            const {error, paymentIntent} = await stripe.confirmCardPayment(
-                clientSecret, {
-                    payment_method: e.paymentMethod.id,
-                }, {
-                    handleActions: false
-                }
-            )
-            if(error) {
-                e.complete("fail")
-                return
-            }
+    pr.on('paymentmethod', async (e) => {
+      const {error: backendError, clientSecret} = await fetch(
+        'https://appletree-express2.herokuapp.com/payment/create-payment-intent',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            paymentMethodType: 'card',
+            currency: 'usd',
+          }),
+        }
+      ).then((r) => r.json());
 
-            e.complete("success")
-            if(paymentIntent.status == "requires_action") {
-                stripe.confirmCardPayment(clientSecret)
-            }
-        })
+      if (backendError) {
+        addMessage(backendError.message);
+        return;
+      }
 
-    }, [stripe, elements])
+      addMessage('Client secret returned');
 
-    return (
-        <>
-            {paymentRequest && <PaymentRequestButtonElement options={{paymentRequest}} />}
-        </>
-    )
-}
+      const {
+        error: stripeError,
+        paymentIntent,
+      } = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: e.paymentMethod.id,
+      }, { handleActions: false });
+
+      if (stripeError) {
+        // Show error to your customer (e.g., insufficient funds)
+        addMessage(stripeError.message);
+        return;
+      }
+
+      // Show a success message to your customer
+      // There's a risk of the customer closing the window before callback
+      // execution. Set up a webhook or plugin to listen for the
+      // payment_intent.succeeded event that handles any business critical
+      // post-payment actions.
+      addMessage(`Payment ${paymentIntent.status}: ${paymentIntent.id}`);
+    });
+  }, [stripe, elements, addMessage]);
+
+  return (
+    <>
+      <h1>Apple Pay</h1>
+
+      <p>
+        Before you start, you need to:
+        <ul>
+          <li><a href="https://stripe.com/docs/stripe-js/elements/payment-request-button#html-js-testing" target="_blank">Add a payment method to your browser.</a> For example, add a card to your Wallet for Safari.</li>
+          <li>Serve your application over HTTPS. This is a requirement both in development and in production. One way to get up and running is to use a service like <a href="https://ngrok.com/" target="_blank" rel="noopener noreferrer">ngrok</a>.</li>
+          <li><a href="https://stripe.com/docs/stripe-js/elements/payment-request-button#verifying-your-domain-with-apple-pay" target="_blank">Verify your domain with Apple Pay</a>, both in development and production.</li>
+        </ul>
+      </p>
+
+      <a href="https://stripe.com/docs/stripe-js/elements/payment-request-button" target="_blank">Stripe Documentation</a>
+
+      {paymentRequest && <PaymentRequestButtonElement options={{paymentRequest}} />}
+
+      <StatusMessages messages={messages} />
+
+      <p> <a href="https://youtu.be/bMCsJfJyQKA" target="_blank">Watch a demo walkthrough</a> </p>
+    </>
+  );
+};
+
+export default withRouter(ApplePay);
